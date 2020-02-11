3294f9f0d89d8659f3e99421b6881ad1bc726ec5
tritainguyen98 <56943599+tritainguyen98@users.noreply.github.com>
2020-02-11 11:25:15 -0800

    updated multiplier testbench and multiplier

--- tb_multiplier.v
+++ tb_multiplier.v
@@ -21,6 +21,7 @@
 
 
 module tb_multiplier #(parameter DATA_WIDTH=32, parameter KERNEL_SIZE=3) ();
+    
     // Inputs
     reg [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] r_weights;
     reg [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] r_pixel_data;
@@ -36,6 +37,29 @@ module tb_multiplier #(parameter DATA_WIDTH=32, parameter KERNEL_SIZE=3) ();
         .pixel_data(r_pixel_data),
         .result(w_result)
     );    
+    
+//    reg [DATA_WIDTH - 1:0] a;
+//    reg [DATA_WIDTH - 1:0] b;
+    
+//    assign r_weights = a;
+//    assign r_pixel_data = b;
 
+    integer i, j;
+    
+    initial begin
+    for( i = 0; i < (KERNEL_SIZE**2); i = i + 1)
+        begin
+            r_weights [i*(DATA_WIDTH-1) +: DATA_WIDTH-1]  = $random;
+        end
+     end
+   
+    initial begin
+    for ( j = 0; j < 100; j = 1 + j)
+        begin 
+            r_pixel_data [ j*(DATA_WIDTH-1) +: DATA_WIDTH-1] = $random;
+            #50;
+            $display("A: %d, B: %d, OUTPUT: %d", r_weights, r_pixel_data, w_result); 
+        end
+    end
 
 endmodule
