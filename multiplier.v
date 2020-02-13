570537d4ab208414a287b2491cfea9d054d7c394
ihflores <58196917+ihflores@users.noreply.github.com>
2020-02-13 12:54:53 -0800

    Added fixed point to multiplier
    Output not working as of now. The ourput is unknown and reason for this error is unknown as well.

--- multiplier.v
+++ multiplier.v
@@ -1,21 +1,24 @@
 `timescale 1ns / 1ps
 
-
-module multiplier #(parameter DATA_WIDTH=32, parameter KERNEL_SIZE=3) (
+module multiplier #(parameter DATA_WIDTH=16, FRAC_BIT = 8, parameter KERNEL_SIZE=5) (
     // Inputs
-    input [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] weights,
-    input [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] pixel_data,
+    input signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] weights,
+    input signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] pixel_data,
     
     // Output
-    output [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] result
+    output signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] result
     );
+    
+    //Non register register
+    wire signed [DATA_WIDTH*2 - 1:0] temp;
     
     // assign multipliers
     genvar i;
     generate
         for (i = 0; i < (KERNEL_SIZE**2); i = i + 1)
         begin
-            assign result[i*DATA_WIDTH +: DATA_WIDTH] = $signed(weights[i*DATA_WIDTH +: DATA_WIDTH]) * $signed(pixel_data[i*DATA_WIDTH +: DATA_WIDTH]);
+            assign temp = (weights[i*DATA_WIDTH +: DATA_WIDTH]) * (pixel_data[i*DATA_WIDTH +: DATA_WIDTH]);
+            assign result[i*DATA_WIDTH +: DATA_WIDTH] = {temp[DATA_WIDTH*2 - 1], temp[DATA_WIDTH + FRAC_BIT - 1:FRAC_BIT]};
         end
     endgenerate
     
