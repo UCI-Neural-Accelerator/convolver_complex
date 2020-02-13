a03bc6aa3d1c9ddaa6ee4296341e220d688a8931
ihflores <58196917+ihflores@users.noreply.github.com>
2020-02-13 13:19:07 -0800

    Maybe fixed, didin't check lol
    potential fix, not checked yet

--- multiplier.v
+++ multiplier.v
@@ -10,15 +10,16 @@ module multiplier #(parameter DATA_WIDTH=16, parameter FRAC_BIT = 8, parameter K
     );
     
     //Non register register
-    wire signed [DATA_WIDTH*2 - 1:0] temp;
+    wire signed [(KERNEL_SIZE**2)*DATA_WIDTH*2 - 1:0] temp;
     
     // assign multipliers
     genvar i;
     generate
         for (i = 0; i < (KERNEL_SIZE**2); i = i + 1)
         begin
-            assign temp = (weights[i*DATA_WIDTH +: DATA_WIDTH]) * (pixel_data[i*DATA_WIDTH +: DATA_WIDTH]);
-            assign result[i*DATA_WIDTH +: DATA_WIDTH] = {temp[DATA_WIDTH*2 - 1], temp[DATA_WIDTH + FRAC_BIT - 1:FRAC_BIT]};
+            assign temp[i*DATA_WIDTH*2 +: DATA_WIDTH*2] = (weights[i*DATA_WIDTH +: DATA_WIDTH]) * (pixel_data[i*DATA_WIDTH +: DATA_WIDTH]);
+            assign result[i*DATA_WIDTH +: DATA_WIDTH] = {temp[i*DATA_WIDTH*2 - 1], temp[i*DATA_WIDTH*2 - FRAC_BIT - 1:FRAC_BIT*i]}; //31:0 [23:8] 63:32 [55:40]
+            //[63,62,61,60,59,58,57,56,|55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40|,39,38,37,36,35,34,33,32]
         end
     endgenerate
     
