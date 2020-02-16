3310b8ca6c12a443006f19fd40b041ceeed46dce
tritainguyen98 <56943599+tritainguyen98@users.noreply.github.com>
2020-02-15 17:21:28 -0800

    updated adder tree and tb for 16bit fixed point values

--- adder_tree.v
+++ adder_tree.v
@@ -1,6 +1,6 @@
 `timescale 1ns/1ps
 
-module adder_tree #(parameter DATA_WIDTH = 32)(
+module adder_tree #(parameter DATA_WIDTH = 16)(
     // inputs
     input signed [DATA_WIDTH-1:0] data_in_0,
     input signed [DATA_WIDTH-1:0] data_in_1,
@@ -18,18 +18,14 @@ module adder_tree #(parameter DATA_WIDTH = 32)(
     );
     
     // wire
-    wire signed [DATA_WIDTH - 1:0] inter_0_0;
-    wire signed [DATA_WIDTH - 1:0] inter_0_1;
-    wire signed [DATA_WIDTH - 1:0] inter_0_2;
-    wire signed [DATA_WIDTH - 1:0] inter_0_3;
-    wire signed [DATA_WIDTH - 1:0] inter_0_4;
-    wire signed [DATA_WIDTH - 1:0] inter_1_0;
-    wire signed [DATA_WIDTH - 1:0] inter_1_1;
-    wire signed [DATA_WIDTH - 1:0] inter_2_0;
-    
-    // Register reg
-    
-    // Non-register reg
+    wire signed [DATA_WIDTH-1:0] inter_0_0;
+    wire signed [DATA_WIDTH-1:0] inter_0_1;
+    wire signed [DATA_WIDTH-1:0] inter_0_2;
+    wire signed [DATA_WIDTH-1:0] inter_0_3;
+    wire signed [DATA_WIDTH-1:0] inter_0_4;
+    wire signed [DATA_WIDTH-1:0] inter_1_0;
+    wire signed [DATA_WIDTH-1:0] inter_1_1;
+    wire signed [DATA_WIDTH-1:0] inter_2_0;
     
     // first stage
     assign inter_0_0 = data_in_0 + data_in_1;
@@ -37,7 +33,7 @@ module adder_tree #(parameter DATA_WIDTH = 32)(
     assign inter_0_2 = data_in_4 + data_in_5;
     assign inter_0_3 = data_in_6 + data_in_7;
     assign inter_0_4 = data_in_8 + bias;
-    
+
     // second stage
     assign inter_1_0 = inter_0_0 + inter_0_1;
     assign inter_1_1 = inter_0_2 + inter_0_3;
@@ -47,5 +43,6 @@ module adder_tree #(parameter DATA_WIDTH = 32)(
     
     // fourth stage
     assign result = inter_2_0 + inter_0_4;   
+    
     
 endmodule
--- tb_adder_tree.v
+++ tb_adder_tree.v
@@ -21,9 +21,10 @@
 
 
 module tb_adder_tree();
-    parameter DATA_WIDTH = 32;
+    parameter DATA_WIDTH = 16;
     parameter CLK_PERIOD = 20;
     parameter TEST_ITERATIONS = 10;
+    parameter sf = 2.0 ** -8.0;
     
     // inputs
     reg signed [DATA_WIDTH-1:0] r_data_in_0;
@@ -42,7 +43,7 @@ module tb_adder_tree();
     
     
     // instantiate the adder tree
-    adder_tree #(.DATA_WIDTH(DATA_WIDTH)) uut
+    adder_tree #(.DATA_WIDTH(DATA_WIDTH), .sf(sf)) uut
     (
         .data_in_0(r_data_in_0),
         .data_in_1(r_data_in_1),
@@ -57,13 +58,17 @@ module tb_adder_tree();
         .result(w_result)        
     );
     
+    reg signed [DATA_WIDTH - 1:0] inter1;
+    reg signed [DATA_WIDTH - 1:0] inter2;
+    reg signed [DATA_WIDTH - 1:0] inter_result;
+
     // data generation
     integer i;
     initial
     begin
         for (i = 0; i < TEST_ITERATIONS; i = i + 1)
         begin        
-            // drive inputs to random values
+            // drive inputs to random
             r_data_in_0 = $random;
             r_data_in_1 = $random;
             r_data_in_2 = $random;
@@ -75,36 +80,48 @@ module tb_adder_tree();
             r_data_in_8 = $random;
             r_bias = $random;
             
+            inter1 = r_data_in_0 + r_data_in_1 + r_data_in_2 + r_data_in_3 + 
+            r_data_in_4 + r_data_in_5 + r_data_in_6 + r_data_in_7;
             
+            inter2 = r_data_in_8 + r_bias;
+    
+            inter_result = inter1 + inter2;
             // delay
             #(CLK_PERIOD);
             
-            if (w_result == r_data_in_0 +
-                            r_data_in_1 +
-                            r_data_in_2 +
-                            r_data_in_3 +
-                            r_data_in_4 +
-                            r_data_in_5 +
-                            r_data_in_6 +
-                            r_data_in_7 +
-                            r_data_in_8 +
-                            r_bias)
+            if (w_result == inter_result)
             begin
-                $display("Add sucessfull\n");
+                $display("Iteration%d: Successfully added:\n%f \n%f \n%f \n%f \n%f \n%f \n%f \n%f \n%f \n%f \nResult:%f\n",
+                            i,
+                            $itor(r_data_in_0) * sf,
+                            $itor(r_data_in_1) * sf,
+                            $itor(r_data_in_2) * sf,
+                            $itor(r_data_in_3) * sf,
+                            $itor(r_data_in_4) * sf,
+                            $itor(r_data_in_5) * sf,
+                            $itor(r_data_in_6) * sf,
+                            $itor(r_data_in_7) * sf,
+                            $itor(r_data_in_8) * sf,
+                            $itor(r_bias) * sf,
+                            $itor(inter_result[DATA_WIDTH:1]) * sf
+                            );            
             end
             else
             begin
-                $display("Failed to add:\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n%h\n",
-                            r_data_in_0,
-                            r_data_in_1,
-                            r_data_in_2,
-                            r_data_in_3,
-                            r_data_in_4,
-                            r_data_in_5,
-                            r_data_in_6,
-                            r_data_in_7,
-                            r_data_in_8,
-                            r_bias);
+                $display("Iteration%d: Failed to add:\n%f \n%f \n%f \n%f \n%f \n%f \n%f \n%f \n%f \n%f \nResult:%f\n",
+                            i,
+                            $itor(r_data_in_0) * sf,
+                            $itor(r_data_in_1) * sf,
+                            $itor(r_data_in_2) * sf,
+                            $itor(r_data_in_3) * sf,
+                            $itor(r_data_in_4) * sf,
+                            $itor(r_data_in_5) * sf,
+                            $itor(r_data_in_6) * sf,
+                            $itor(r_data_in_7) * sf,
+                            $itor(r_data_in_8) * sf,
+                            $itor(inter_result[DATA_WIDTH:1]) * sf,
+                            $itor(r_bias) * sf
+                            );
             end
                        
         end
