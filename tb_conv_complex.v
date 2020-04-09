c64496b0cd77477b9272d9bee8b3156c2bea3005
ihflores <ianhawkflores@gmail.com>
2020-04-09 12:58:01 -0700

    started file IO test bench with multiplier and adder
    stared on the test bench to test the multiplier and adder with an input file

--- tb_conv_complex.v
+++ tb_conv_complex.v
@@ -5,24 +5,16 @@ module tb_conv_complex();
 parameter BW = 16;
 parameter  FRAC_BIT = 8;
 parameter KERN_DIM = 5;
-//parameter WIDTH = 28;
-//parameter POOL_WIDTH = 24;
-//parameter NUM_CHAN_IN = 1;
-//parameter NUM_CHAN = 20;
-//parameter CHAN_BW = $clog2(NUM_CHAN);
 
 parameter KERN_SIZE = KERN_DIM * KERN_DIM;
 
 parameter HALF_CLK_DURATION = 5;
 parameter RESET_DURATION = 18;
-//parameter PROBE_SLACK = 0.5;
 
 reg clk;
 reg rstn;
 
-//reg [CHAN_BW-1:0] chan;
 reg enable;
-//reg input_en;
 reg signed [BW-1:0] iPixel;
 wire [BW*KERN_SIZE-1:0] weights;
 wire signed [BW-1:0] bias;
@@ -44,12 +36,12 @@ initial begin
     rstn = 'd1;
 end
 
-integer tmpchan;
-integer filechan;
-initial begin
-    filechan = $fopen("chan.in", "r");
-    tmpchan = $fscanf(filechan, "%d", chan);
-end
+//integer tmpchan;
+//integer filechan;
+//initial begin
+//    filechan = $fopen("chan.in", "r");
+//    tmpchan = $fscanf(filechan, "%d", chan);
+//end
 
 convolver_complex uut 
 (
@@ -57,8 +49,6 @@ convolver_complex uut
     .reset(rstn),
     .weight_write(weight_write)
 );
-
-//need to add more here? look at min soo's design, he has more modules here
 
 
 
