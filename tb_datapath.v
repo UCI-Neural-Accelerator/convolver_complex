4e592a4eed7c328f0bea626aece2f781c889db8e
Noelia Cívico Dorado <noeliacivico@gmail.com>
2020-04-20 19:24:38 -0700

    updated tb_datapath, needs to continue being tested

--- tb_datapath.v
+++ tb_datapath.v
@@ -8,11 +8,14 @@ module tb_datapath();
     parameter KERNEL_SIZE = 5;
     parameter IMAGE_SIZE = 28;
 	
-    parameter CLK_PERIOD = 20;
+//    parameter CLK_PERIOD = 20;
 //    parameter TEST_ITERATIONS = 10;
 //    parameter sf = 2.0 ** -8.0;
 	
     // Inputs
+    reg clk;
+    reg reset;
+    reg write;
     reg signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] r_weights;
     reg signed [DATA_WIDTH - 1:0] r_pixel_input;
     reg signed [DATA_WIDTH - 1:0] bias;
@@ -24,28 +27,16 @@ module tb_datapath();
     wire signed [DATA_WIDTH - 1:0] final_result;
    
     datapath #(.DATA_WIDTH(DATA_WIDTH),.KERNEL_SIZE(KERNEL_SIZE), .FRAC_BIT(FRAC_BIT)) datapath (
-        .clk(),
-        .reset(),
-		.write(),
+        .clk(clk),
+        .reset(reset),
+		.write(write),
         .bias(bias),
         .weights(r_weights),
         .pixel_input(r_pixel_input),
         .add_result(final_result)
     );
 
-    integer i;
-    initial
-        begin
-    
-        r_pixel_input = 16'b00000010_00000000;
-        bias = 16'h0000;
-    
-        for ( i = 0; i < KERNEL_SIZE**2; i = i + 1)
-        begin
-            r_weights[i*DATA_WIDTH +: DATA_WIDTH] = 16'b00000001_00000000;
-        end
-        
-        #(CLK_PERIOD / 2);        
+//        #(CLK_PERIOD / 2);        
         
 //        if (final_result == 16'h3200)
 //        begin
@@ -57,7 +48,44 @@ module tb_datapath();
 //            $display("Failed to add. Result: %d\n", final_result * sf);
 
 //        end
+
+    initial
+    begin
+        clk = 0;
+        write = 1;
+        reset = 1;
+        #100
+        reset = 0;
+       
+        forever #20 clk = ~clk;
     
     end
+
+    integer i;
+    initial
+    begin
+    
+        for ( i = 0; i < KERNEL_SIZE**2; i = i + 1)
+        begin
+            r_weights[i*DATA_WIDTH +: DATA_WIDTH] = 16'b00000001_00000000;  
+        end
+    
+    end
+    
+    integer j;
+    initial
+    begin
+    
+        bias = 16'h0000;
+        r_pixel_input = 16'b00000001_00000000;
+
+//        for ( j = 0; j < KERNEL_SIZE; j = j + 1)
+//        begin
+//            r_pixel_input = 16'b00000001_00000000;  
+//        end
+        
+//        r_pixel_input = 16'b00000000_00000000;
+ 
+     end
     
 endmodule
