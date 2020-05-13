c52dd6fa908f95bff9b6b6cfaa3da0693e077bfc
Noelia Cívico Dorado <noeliacivico@gmail.com>
2020-05-12 19:00:11 -0700

    updated without verification outputs

--- convolver_complex.v
+++ convolver_complex.v
@@ -7,30 +7,31 @@ module convolver_complex #(parameter DATA_WIDTH = 16, parameter FRAC_BIT = 8, pa
     	input [DATA_WIDTH - 1:0] bias,
 	
     	output wire [DATA_WIDTH-1:0] conv_final_result,
-        output wire enable_signal,
-	// outputs for verification
-    	output wire signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] v_weights_out,
-	output wire signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] v_data_out_0,
-	output wire signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] v_data_out_1,
-	output wire signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] v_data_out_2,
-	output wire signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] v_data_out_3,
-	output wire signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] v_data_out_4,
+        output wire enable_signal
+		
+		// outputs for verification while simulating
+/*     	output wire signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] v_weights_out,
+		output wire signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] v_data_out_0,
+		output wire signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] v_data_out_1,
+		output wire signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] v_data_out_2,
+		output wire signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] v_data_out_3,
+		output wire signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] v_data_out_4,
     	output wire signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] v_mult_result,
         output wire signed [ ((IMAGE_SIZE-KERNEL_SIZE)*DATA_WIDTH)- 1:0] v_data_hold_0,
         output wire signed [ ((IMAGE_SIZE-KERNEL_SIZE)*DATA_WIDTH)- 1:0] v_data_hold_1,
     	output wire signed [ ((IMAGE_SIZE-KERNEL_SIZE)*DATA_WIDTH)- 1:0] v_data_hold_2,
-    	output wire signed [ ((IMAGE_SIZE-KERNEL_SIZE)*DATA_WIDTH)- 1:0] v_data_hold_3
+    	output wire signed [ ((IMAGE_SIZE-KERNEL_SIZE)*DATA_WIDTH)- 1:0] v_data_hold_3 */
     );
 
     datapath #(.DATA_WIDTH(DATA_WIDTH),.KERNEL_SIZE(KERNEL_SIZE), .FRAC_BIT(FRAC_BIT), .IMAGE_SIZE(IMAGE_SIZE)) datapath_unit (
         .clk(clk),
         .reset(reset),
-	.write(write),
+		.write(write),
         .bias(bias),
         .weights(weights_matrix),
         .pixel_input(pixel_in),
-        .add_result(conv_final_result),
-        .weights_out(v_weights_out),
+        .add_result(conv_final_result)
+/*         .weights_out(v_weights_out),
         .data_out_0(v_data_out_0),
         .data_out_1(v_data_out_1),
         .data_out_2(v_data_out_2),
@@ -40,7 +41,7 @@ module convolver_complex #(parameter DATA_WIDTH = 16, parameter FRAC_BIT = 8, pa
         .data_hold_0(v_data_hold_0),
         .data_hold_1(v_data_hold_1),
         .data_hold_2(v_data_hold_2),
-        .data_hold_3(v_data_hold_3)
+        .data_hold_3(v_data_hold_3) */
     );
         
     controlpath #(.KERNEL_SIZE(KERNEL_SIZE), .DATA_WIDTH(DATA_WIDTH), .IMAGE_SIZE(IMAGE_SIZE)) controlpath_unit (
--- datapath.v
+++ datapath.v
@@ -9,19 +9,21 @@ module datapath #(parameter DATA_WIDTH = 16, parameter FRAC_BIT = 8, parameter K
     	input [DATA_WIDTH - 1:0] bias,
 	
     	// outputs
-    	output signed [DATA_WIDTH-1:0] add_result,
-	// outputs for verification
-	output signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] data_out_0,
-	output signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] data_out_1,
-	output signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] data_out_2,
-	output signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] data_out_3,
-	output signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] data_out_4,
+    	output signed [DATA_WIDTH-1:0] add_result
+		// outputs for verification while simulating
+/* 		output signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] data_out_0,
+		output signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] data_out_1,
+		output signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] data_out_2,
+		output signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] data_out_3,
+		output signed [ (KERNEL_SIZE*DATA_WIDTH)- 1:0] data_out_4,
     	output signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] weights_out,
     	output signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] mult_result,
         output wire signed [ ((IMAGE_SIZE-KERNEL_SIZE)*DATA_WIDTH)- 1:0] data_hold_0,
         output wire signed [ ((IMAGE_SIZE-KERNEL_SIZE)*DATA_WIDTH)- 1:0] data_hold_1,
     	output wire signed [ ((IMAGE_SIZE-KERNEL_SIZE)*DATA_WIDTH)- 1:0] data_hold_2,
-    	output wire signed [ ((IMAGE_SIZE-KERNEL_SIZE)*DATA_WIDTH)- 1:0] data_hold_3
+    	output wire signed [ ((IMAGE_SIZE-KERNEL_SIZE)*DATA_WIDTH)- 1:0] data_hold_3 */ 
+		
+		//data_hold_x need to be added to the instantiated intermediate shift registers modules if uncomment
     );
     
 	// wires of the shift registers connected to multiplier
@@ -41,31 +43,31 @@ module datapath #(parameter DATA_WIDTH = 16, parameter FRAC_BIT = 8, parameter K
 	wire signed [DATA_WIDTH - 1:0] inter_out_3;
 	wire signed [DATA_WIDTH - 1:0] inter_in_4;
 
-    	wire signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] inter_weights;
-    	wire signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] pixel_data;	
-    	wire signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] m_result;
-      
-    	// concatenate shift register outputs for multiplier unit
-    	assign pixel_data = {w_data_out_0, w_data_out_1, w_data_out_2, w_data_out_3, w_data_out_4};
+	wire signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] inter_weights;
+	wire signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] pixel_data;	
+	wire signed [(KERNEL_SIZE**2)*DATA_WIDTH - 1:0] m_result;
+  
+	// concatenate shift register outputs for multiplier unit
+	assign pixel_data = {w_data_out_0, w_data_out_1, w_data_out_2, w_data_out_3, w_data_out_4};
 
-	// assign for verification
-	assign weights_out = inter_weights;
+	// assignments for verification while simulating
+/* 	assign weights_out = inter_weights;
 	assign data_out_0 = w_data_out_0;
 	assign data_out_1 = w_data_out_1;
 	assign data_out_2 = w_data_out_2;
 	assign data_out_3 = w_data_out_3;
 	assign data_out_4 = w_data_out_4;
-	assign mult_result = m_result;
+	assign mult_result = m_result; */
 
-	parameter INTER_SIZE = (IMAGE_SIZE - KERNEL_SIZE);
+	parameter INTER_SIZE = (IMAGE_SIZE - KERNEL_SIZE); //size of intermediate shift registers
     
-    	weight_register #(.DATA_WIDTH(DATA_WIDTH), .N(KERNEL_SIZE**2)) weight_register_0 (
-            .reset(reset),
-            .clock(clk),
-            .write(write),
-            .weight_write(weights),
-            .weight_read(inter_weights)
-        );
+	weight_register #(.DATA_WIDTH(DATA_WIDTH), .N(KERNEL_SIZE**2)) weight_register_0 (
+		.reset(reset),
+		.clock(clk),
+		.write(write),
+		.weight_write(weights),
+		.weight_read(inter_weights)
+	);
 
 	shift_register #(.SIZE(KERNEL_SIZE), .DATA_WIDTH(DATA_WIDTH)) shift_register_0 (
 			.shift_in(pixel_input),
@@ -112,7 +114,7 @@ module datapath #(parameter DATA_WIDTH = 16, parameter FRAC_BIT = 8, parameter K
 			.clock(clk),
 			.reset(reset),
 			.shift_out(inter_in_1),
-			.data_out(data_hold_0)
+			.data_out()
 		);
 
 	shift_register #(.SIZE(INTER_SIZE), .DATA_WIDTH(DATA_WIDTH)) inter_register_1 (
@@ -120,7 +122,7 @@ module datapath #(parameter DATA_WIDTH = 16, parameter FRAC_BIT = 8, parameter K
 			.clock(clk),
 			.reset(reset),
 			.shift_out(inter_in_2),
-			.data_out(data_hold_1)
+			.data_out()
 		);
 
 	shift_register #(.SIZE(INTER_SIZE), .DATA_WIDTH(DATA_WIDTH)) inter_register_2 (
@@ -128,7 +130,7 @@ module datapath #(parameter DATA_WIDTH = 16, parameter FRAC_BIT = 8, parameter K
 			.clock(clk),
 			.reset(reset),
 			.shift_out(inter_in_3),
-			.data_out(data_hold_2)
+			.data_out()
 		);
 
 	shift_register #(.SIZE(INTER_SIZE), .DATA_WIDTH(DATA_WIDTH)) inter_register_3 (
@@ -136,46 +138,46 @@ module datapath #(parameter DATA_WIDTH = 16, parameter FRAC_BIT = 8, parameter K
 			.clock(clk),
 			.reset(reset),
 			.shift_out(inter_in_4),
-			.data_out(data_hold_3)
+			.data_out()
 		);
 		
         
-     	multiplier #(.FRAC_BIT(FRAC_BIT), .DATA_WIDTH(DATA_WIDTH), .KERNEL_SIZE(KERNEL_SIZE)) multiplier_0 (
-            	.weights(inter_weights),
-            	.pixel_data(pixel_data),
-			
-            	.result(m_result)
-        );
-     
-    	adder_tree #(.DATA_WIDTH(DATA_WIDTH)) adder_tree_0 (
-        	.data_in_0( m_result[0*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_1( m_result[1*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_2( m_result[2*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_3( m_result[3*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_4( m_result[4*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_5( m_result[5*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_6( m_result[6*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_7( m_result[7*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_8( m_result[8*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_9( m_result[9*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_10( m_result[10*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_11( m_result[11*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_12( m_result[12*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_13( m_result[13*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_14( m_result[14*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_15( m_result[15*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_16( m_result[16*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_17( m_result[17*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_18( m_result[18*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_19( m_result[19*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_20( m_result[20*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_21( m_result[21*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_22( m_result[22*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_23( m_result[23*DATA_WIDTH +: DATA_WIDTH] ),
-        	.data_in_24( m_result[24*DATA_WIDTH +: DATA_WIDTH] ),
-        	.bias(bias),
+	multiplier #(.FRAC_BIT(FRAC_BIT), .DATA_WIDTH(DATA_WIDTH), .KERNEL_SIZE(KERNEL_SIZE)) multiplier_0 (
+			.weights(inter_weights),
+			.pixel_data(pixel_data),
 		
-        	.result(add_result)        
-        );  
+			.result(m_result)
+	);
+ 
+	adder_tree #(.DATA_WIDTH(DATA_WIDTH)) adder_tree_0 (
+		.data_in_0( m_result[0*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_1( m_result[1*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_2( m_result[2*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_3( m_result[3*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_4( m_result[4*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_5( m_result[5*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_6( m_result[6*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_7( m_result[7*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_8( m_result[8*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_9( m_result[9*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_10( m_result[10*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_11( m_result[11*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_12( m_result[12*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_13( m_result[13*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_14( m_result[14*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_15( m_result[15*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_16( m_result[16*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_17( m_result[17*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_18( m_result[18*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_19( m_result[19*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_20( m_result[20*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_21( m_result[21*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_22( m_result[22*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_23( m_result[23*DATA_WIDTH +: DATA_WIDTH] ),
+		.data_in_24( m_result[24*DATA_WIDTH +: DATA_WIDTH] ),
+		.bias(bias),
+	
+		.result(add_result)        
+	);  
     
 endmodule
